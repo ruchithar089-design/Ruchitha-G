f0043c9a43fd006707b663307858b3c6241c83ff
Ruchitha G <ruchithar089@gmail.com>
2026-08-26 17:14:17 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-public class Hellojava {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-        System.out.println("Java program executed successfully.");
+        System.out.println("Hello world");
+        
     }
 }
