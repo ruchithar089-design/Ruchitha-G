13e84b671c23a040221fc963f6dccb6bd514165d
Ruchitha G <ruchithar089@gmail.com>
2026-08-26 17:12:26 +0530

    Rename class HelloWorld to Hellojava

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Hellojava {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
         System.out.println("Java program executed successfully.");
